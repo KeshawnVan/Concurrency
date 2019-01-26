593d6e8bed25ac098759dba99f69db6ab8f62970
Fankx <fankx@startimes.com.cn>
2019-01-26 13:48:37 +0800

    NonReentrantLock

--- concurrent/src/main/java/lock/NonReentrantLock.java
+++ concurrent/src/main/java/lock/NonReentrantLock.java
@@ -70,4 +70,39 @@ public class NonReentrantLock implements Lock, Serializable {
     public Condition newCondition() {
         return sync.newCondition();
     }
+
+    public static void main(String[] args) throws Exception {
+        Lock lock = new NonReentrantLock();
+        Runnable runnable = () -> {
+            System.out.println(Thread.currentThread() + " begin lock");
+            lock.lock();
+            System.out.println(Thread.currentThread() + " get lock");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread() + " begin release lock");
+            lock.unlock();
+            System.out.println(Thread.currentThread() + " release lock");
+        };
+
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+        Thread thread4 = new Thread(runnable);
+        Thread thread5 = new Thread(runnable);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        thread4.join();
+        thread5.join();
+    }
 }
